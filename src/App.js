4a9f1199acd4a37fa108c91d9fbb5d96968886af
renzrph <renzestorrph@gmail.com>
2024-03-07 14:56:50 +0800

    feat: hero section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+
+// import aos
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+// import components
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  // aos init
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  });
+
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
