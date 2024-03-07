36d7920b7c1d4fbca4f6397ed520077d974a2d18
renzrph <renzestorrph@gmail.com>
2024-03-07 16:46:31 +0800

    feat: header section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 
 // import aos
@@ -6,9 +7,14 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 // import components
+import Header from "./components/Header";
 import Hero from "./components/Hero";
+import NavMobile from "./components/NavMobile";
 
 const App = () => {
+  // mobile nav state
+  const [navMobile, setNavMobile] = useState(true);
+
   // aos init
   useEffect(() => {
     Aos.init({
@@ -19,7 +25,16 @@ const App = () => {
 
   return (
     <div className="overflow-hidden">
+      <Header setNavMobile={setNavMobile} />
       <Hero />
+      {/* mobile nav */}
+      <div
+        className={`${
+          navMobile ? "right-0" : "-right-full"
+        } fixed z-10 top-0 h-full transition all duration-200`}
+      >
+        <NavMobile setNavMobile={setNavMobile} />
+      </div>
     </div>
   );
 };
