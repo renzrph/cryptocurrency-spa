62b7413fe098ac9d4845fd9e4b7db220e34d6c3a
renzrph <renzestorrph@gmail.com>
2024-03-08 09:30:49 +0800

    feat: Newsletter components

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Why from "./components/Why";
 import Calculate from "./components/Calculate";
 import Trade from "./components/Trade";
 import Features from "./components/Features";
+import Newsletter from "./components/Newsletter";
 
 const App = () => {
   // mobile nav state
@@ -45,6 +46,7 @@ const App = () => {
       <Calculate />
       <Trade />
       <Features />
+      <Newsletter />
       <div className="h-[2000px]"></div>
     </div>
   );
